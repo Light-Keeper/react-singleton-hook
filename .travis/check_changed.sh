3979ad8565dd6d78e1d172caf868f11adfef682a
Vadim Yanushkevich <vadik.ya@gmail.com>
2020-03-19 00:25:03 +0200

    configure build

--- .travis/check_changed.sh
+++ .travis/check_changed.sh
@@ -1,9 +1,6 @@
 CHANGED_FILES=$(git diff --name-status HEAD~1...HEAD .)
 if [ -z "$CHANGED_FILES" ]
 then
-      echo "No changes in $(pwd)"
-      exit 137
+  exit 137
 fi
-
-echo "There were changes in $LOCATION changes:  $(pwd)"
 exit 0
