aa8454ef22a5dbf40c4a40c2c404e606d0573f1c
Vadim Yanushkevich <vadik.ya@gmail.com>
2021-05-23 22:22:30 +0300

    awoind using window

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,21 +1,19 @@
 import React from 'react';
+/* eslint-disable import/no-unresolved */
+import { unstable_batchedUpdates, render } from 'react-dom';
 import { warning } from './warning';
 
-let unstable_batchedUpdates, render;
+// from https://github.com/purposeindustries/window-or-global/blob/master/lib/index.js
+// avoid direct usage of 'window' because `window is not defined` error might happen in babel-node
+const globalObject = (typeof self === 'object' && self.self === self && self)
+  || (typeof global === 'object' && global.global === global && global)
+  || this;
 
-try {
-  const reactDom = require('react-dom');
-  unstable_batchedUpdates = reactDom.unstable_batchedUpdates;
-  render = reactDom.render;
-} catch (e) {
-  unstable_batchedUpdates = cb => cb();
-  render = () => { throw new Error('can not render without react-dom. Mount SingletonHooksContainer manually'); };
-}
 
 export const batch = cb => unstable_batchedUpdates(cb);
 export const mount = C => {
-  if (window && window.document) {
-    render(<C/>, window.document.createElement('div'));
+  if (globalObject.document && globalObject.document.createElement) {
+    render(<C/>, globalObject.document.createElement('div'));
   } else {
     warning('Can not mount SingletonHooksContainer on server side. '
       + 'Did you manage to run useEffect on server? '
