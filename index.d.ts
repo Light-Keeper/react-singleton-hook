0591598c2ef09e7e4bafb519ef569a410f1bb8fa
Ying Zhe Chong <zh3zh3@gmail.com>
2021-07-01 23:00:18 -0700

    Add generic parameters for useHook function

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,6 @@ export = ReactSingletonHook;
 export as namespace ReactSingletonHook;
 
 declare namespace ReactSingletonHook {
-  function singletonHook<ValueType>(initialState: ValueType | (() => ValueType), useHook: () => ValueType): () => ValueType;
+  function singletonHook<ValueType, Args extends any[]>(initialState: ValueType | (() => ValueType), useHook: (...args: Args) => ValueType): (...args: Args) => ValueType;
   function SingletonHooksContainer(): any;
 }
