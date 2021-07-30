e3250a6d11b75a773ad56149e8e1eb752a89404a
David Fourie <david.fourie@stonethree.com>
2021-07-30 11:28:35 +0200

    Add export for state reset

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,17 @@
-
 export = ReactSingletonHook;
 export as namespace ReactSingletonHook;
 
 declare namespace ReactSingletonHook {
-  function singletonHook<ValueType>(initialState: ValueType | (() => ValueType), useHook: () => ValueType): () => ValueType;
-  function SingletonHooksContainer(): any;
+  function singletonHook<ValueType>(
+    initialState: ValueType | (() => ValueType),
+    useHook: () => ValueType
+  ): () => ValueType;
+
+  function SingletonHooksContainer(): JSX.Element;
+  /**
+   * Callback that resets the local state for tests.
+   *
+   * Do not use in production!
+   */
+  function resetLocalStateForTests(): void;
 }
